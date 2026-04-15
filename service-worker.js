c63e4484e00bf7224e0927b30dc807f59f7b8ed2
BigHarryTexan <44851648+BigHarryTexan@users.noreply.github.com>
2026-04-15 15:19:00 -0500

    Refactor service worker for immediate activation
    Removed caching logic and added skipWaiting and clients.claim for immediate activation.

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,7 @@
-const CACHE_NAME = "time-announcer-v1";
-
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-  ];
-
-self.addEventListener("install",event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-    );
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-    );
+self.addEventListener("activate", event => {
+  clients.claim();
 });
